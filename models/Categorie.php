3e121388aa9bdb864dcbe9391e496757d1b8013b
TekGeek_dev <TekGeek_dev@protonmail.com>
2025-03-03 22:14:31 -0500

    modification name file CRUD

--- models/Categorie.php
+++ models/Categorie.php
@@ -3,7 +3,7 @@
 namespace App\Models;
 use App\Models\CRUD;
 
-class Utilisateur extends CRUD{
+class Categorie extends CRUD{
     protected $table = 'categorie';
     protected $primaryKey = 'id';
     protected $fillable = ['nom'];
